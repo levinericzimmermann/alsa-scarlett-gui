9c2f9e4aa3bf3752ac313e585df6ae29eafac683
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-10-23 17:47:53 +0200

    add nix derivation

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs {}}:
+with pkgs;
+
+stdenv.mkDerivation {
+    name = "alsa-scarlett-gui";
+    src = ./src;
+    nativeBuildInputs = [ wrapGAppsHook pkg-config ];
+    propagatedBuildInputs = [
+        gnumake
+        alsa-lib
+        gtk4
+        pkg-config
+        openssl
+    ];
+    buildPhase = ''
+      NIX_CFLAGS_COMPILE="$(pkg-config --cflags gtk4) $NIX_CFLAGS_COMPILE"
+    '';
+    makeFlags = [
+      "DESTDIR=$(out)"
+      "-j4"
+    ];
+}
